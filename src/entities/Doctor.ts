19ea5f430c863cccd53a6c63adabe0dadaa85bc7
igkaiiii <157233858+igkaiiii@users.noreply.github.com>
2024-10-08 20:03:30 -0400

    Update Doctor.ts

--- src/entities/Doctor.ts
+++ src/entities/Doctor.ts
@@ -1,1 +1,10 @@
+export interface IDoctor {
+  id: string;
+  name: string;
+  specialty: string;
+}
+
+export class Doctor implements IDoctor {
+  constructor(public id: string, public name: string, public specialty: string) {}
+}
 
