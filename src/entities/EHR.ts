d054e7371922b7563d30958097caa45e0a211276
igkaiiii <157233858+igkaiiii@users.noreply.github.com>
2024-10-08 20:04:28 -0400

    Update EHR.ts

--- src/entities/EHR.ts
+++ src/entities/EHR.ts
@@ -1,1 +1,18 @@
+export interface IPrescription {
+  id: string;
+  patientId: string;
+  doctorId: string;
+  medication: string;
+  dosage: string;
+}
+
+export class Prescription implements IPrescription {
+  constructor(
+    public id: string,
+    public patientId: string,
+    public doctorId: string,
+    public medication: string,
+    public dosage: string
+  ) {}
+}
 
