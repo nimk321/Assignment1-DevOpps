eca65c9c313961010b7d7c26f143fec80d259ae6
igkaiiii <157233858+igkaiiii@users.noreply.github.com>
2024-10-08 20:08:24 -0400

    Update TelemedicineCommunication.ts

--- src/entities/TelemedicineCommunication.ts
+++ src/entities/TelemedicineCommunication.ts
@@ -1,1 +1,18 @@
+export interface ITeleMedicineCommunication {
+  id: string;
+  patientId: string;
+  doctorId: string;
+  date: Date;
+  notes: string;
+}
+
+export class TeleMedicineCommunication implements ITeleMedicineCommunication {
+  constructor(
+    public id: string,
+    public patientId: string,
+    public doctorId: string,
+    public date: Date,
+    public notes: string
+  ) {}
+}
 
