1a868f394b9361d55b774c1fb61fbbe026d410f4
Nim Lal Kandel <c0913081@mylambton.ca>
2024-12-08 14:02:40 -0500

    Revert "Create Patient.test.ts"
    This reverts commit 52a6528f0d0eb9ad1ef64cff258d18a2e4503aa8.

--- __tests__/Patient.test.ts
+++
@@ -1,7 +0,0 @@
-import { Patient } from '../Patient';
-
-test('Should create a new patient', () => {
-  const patient = new Patient('1', 'John Doe');
-  expect(patient.getPatientId()).toBe('1');
-  expect(patient.getName()).toBe('John Doe');
-});
